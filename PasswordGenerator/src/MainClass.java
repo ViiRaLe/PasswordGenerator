9a81972e0de736637eb96307adfb5e58c43a1348
Mattia Pagani <mattiapagani97@gmail.com>
2020-05-21 17:30:53 +0200

    Optimized checks on settings input

--- PasswordGenerator/src/MainClass.java
+++ PasswordGenerator/src/MainClass.java
@@ -9,10 +9,27 @@ public class MainClass
 
         String s = "N";
 
+
         do {
             System.out.println("Insert password length (min. 6), Include Uppercase letters, Include Lowercase letter, Include Numbers, Include Symbols (at least one active).");
 
-            UserSettings set = new UserSettings(kb.nextInt(), kb.nextBoolean(), kb.nextBoolean(), kb.nextBoolean(), kb.nextBoolean());
+            int len = kb.nextInt();
+            String upper = kb.next();
+            String lower = kb.next();
+            String numb = kb.next();
+            String symb = kb.next();
+
+            boolean up = false;
+            boolean low = false;
+            boolean n = false;
+            boolean sy = false;
+
+            if (checkSpelling(upper)) up = true;
+            if (checkSpelling(lower)) low = true;
+            if (checkSpelling(numb)) n = true;
+            if (checkSpelling(symb)) sy = true;
+
+            UserSettings set = new UserSettings(len, up, low, n, sy);
             PasswordGenerator pg = new PasswordGenerator(set);
 
             System.out.println("Version " + pg.getVersion());
@@ -26,9 +43,16 @@ public class MainClass
             }
 
             kb.nextLine();
-            System.out.println("Want to generate another one? Y / N");
+            System.out.println("Want to generate another password? Y / N");
             s = kb.nextLine();
 
         } while (s.equalsIgnoreCase("Y"));
+    }
+
+    private static boolean checkSpelling(String s)
+    {
+        if (s.equals("1") || s.equalsIgnoreCase("true") || s.equalsIgnoreCase("t") || s.equalsIgnoreCase("y") || s.equalsIgnoreCase("yes")) return true;
+
+        return false;
     }
 }
